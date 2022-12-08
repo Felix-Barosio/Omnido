d018d3ff1e844272aea8cbd8be035a31052cef94
Felix <barosiofelix@gmail.com>
2022-12-08 15:16:52 +0300

    add: GET,GET /todos/:id, PATCH

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,3 +1,47 @@
 class TodosController < ApplicationController
 
+  # rendering error messages
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+ 
+  # GET /todos
+  def index
+    @todos = Todo.all
+    render json: @todos, status: :ok
+  end
+
+  # GET /todos/:id
+  def show
+    @todo = find_todo
+    render json: @todo, status: :ok
+  end
+
+  # POST /todos
+  def create
+    @todo = Todo.create!(todo_params)
+    render json: @todo, status: :created
+  end
+
+  private
+
+  # finding todos by id
+  def find_todo
+    Todo.find(params[:id])
+  end
+
+  # creating a new todo with attributes
+  def todo_params
+    params.permit(:text, :category, :done)
+  end
+
+  # rendering messages when todos not found
+  def render_not_found_response
+    render json: { errors: "Task does not found"}, status: :not_found
+  end
+
+  # render messages for invalid responses
+  def render_unprocessable_entity_response(invalid)
+    render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
+  end
+
 end
